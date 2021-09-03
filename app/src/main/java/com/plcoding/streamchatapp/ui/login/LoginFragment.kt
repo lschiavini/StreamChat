5c407d708924e3ab3974fcf919e612fb116d6a01
Lucas <lschiavini@nelogica.com.br>
2021-09-03 16:06:58 -0300

    Setting up login screen

--- app/src/main/java/com/plcoding/streamchatapp/ui/login/LoginFragment.kt
+++ app/src/main/java/com/plcoding/streamchatapp/ui/login/LoginFragment.kt
@@ -1,4 +1,22 @@
 package com.plcoding.streamchatapp.ui.login
 
-class LoginFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import androidx.viewbinding.ViewBinding
+import com.plcoding.streamchatapp.databinding.FragmentLoginBinding
+import com.plcoding.streamchatapp.ui.BindingFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class LoginFragment: BindingFragment<FragmentLoginBinding>() {
+    override val bindingInflater: (LayoutInflater) -> ViewBinding
+        get() = FragmentLoginBinding::inflate
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
 }
--- app/src/main/java/com/plcoding/streamchatapp/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/plcoding/streamchatapp/ui/login/LoginViewModel.kt
@@ -1,4 +1,49 @@
 package com.plcoding.streamchatapp.ui.login
 
-class LoginViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.plcoding.streamchatapp.util.Constants
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.client.call.await
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val client: ChatClient
+) : ViewModel() {
+
+    private val _loginEvent = MutableSharedFlow<LogInEvent>() // object that can be updated server side
+    val loginEvent = _loginEvent.asSharedFlow() // immutable variable for the viewModel
+
+    private fun isValidUsername(username: String) =
+        username.length > Constants.MIN_USERNAME_LENGTH
+
+    fun connectUser(username: String) {
+        val trimmedUsername = username.trim()
+        viewModelScope.launch {
+            if (isValidUsername(trimmedUsername)) {
+                val result = client.connectGuestUser(
+                    userId = trimmedUsername,
+                    username = trimmedUsername
+                ).await()
+
+                if (result.isError) {
+
+                }
+            }
+        }
+    }
+
+    sealed class LogInEvent {
+        object ErrorInputTooShort : LogInEvent()
+        data class ErrorLogIn(val error: String) : LogInEvent()
+        object Success : LogInEvent()
+    }
+
 }
+
