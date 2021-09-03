3150056a146e6785623be88b804e16063c429492
Lucas <lschiavini@nelogica.com.br>
2021-09-03 16:06:22 -0300

    Setting up DaggerHilt

--- app/src/main/java/com/plcoding/streamchatapp/ChatApplication.kt
+++ app/src/main/java/com/plcoding/streamchatapp/ChatApplication.kt
@@ -1,4 +1,19 @@
 package com.plcoding.streamchatapp
 
-class ChatApplication {
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.offline.ChatDomain
+import javax.inject.Inject
+
+@HiltAndroidApp
+class ChatApplication : Application () {
+
+    @Inject
+    lateinit var client: ChatClient
+
+    override fun onCreate() {
+        super.onCreate()
+        ChatDomain.Builder(client, applicationContext).build()
+    }
 }
